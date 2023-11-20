44081736789fc757b4855be0cf7e6933087c4c21
Hiroaki Mitsuyoshi <thnksfrth3244@gmail.com>
2023-11-20 21:32:35 +0900

    add function

---
+++ rsa.py
@@ -0,0 +1,22 @@
+import random
+from sympy import isprime
+
+def large_prime():
+    
+def generate(keysize):
+    
+def encrypt(pub, message):
+    
+def decrypt(priv, encrypted):
+
+def main():
+    keysize = 1024
+    pub, priv = generate(keysize)
+    
+    message = "sample"
+    encrypted = encrypt(pub, message)
+    
+    decrypted = decrypt(priv, encrypted)
+    
+if __name__ == "__main__":
+    main()
