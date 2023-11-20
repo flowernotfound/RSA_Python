4d7a867d70f8c97c75e3d767ad67bce8d8ea24c4
Hiroaki Mitsuyoshi <thnksfrth3244@gmail.com>
2023-11-20 21:47:44 +0900

    add generate

--- rsa.py
+++ rsa.py
@@ -1,27 +1,40 @@
 import random
 from sympy import isprime
 
-def large_prime():
-    
+def large_prime(keysize):
+    while True:
+        num = random.randbits(keysize)
+        if isprime(num):
+            return num
+        
 def generate(keysize):
+    p = large_prime(keysize)
+    q = large_prime(keysize)
+    n = p * q
+    phi = (p - 1) * (q - 1)
     
-def encrypt(pub, message):
+    e = random.randrange(2, phi)
+    while gcd(e, phi) != 1:
+        e = random.randrange(2, phi)
+    d = pow(e, -1, phi)
     
-def decrypt(priv, encrypted):
+# def encrypt(pub, message):
+    
+# def decrypt(priv, encrypted):
 
 def main():
     keysize = 1024
     pub, priv = generate(keysize)
     
-    message = "sample"
-    encrypted = encrypt(pub, message)
+    # message = "sample"
+    # encrypted = encrypt(pub, message)
     
-    decrypted = decrypt(priv, encrypted)
+    # decrypted = decrypt(priv, encrypted)
     
     print(pub + "\n" + priv)
-    print(message)
-    print(encrypted)
-    print(decrypted)
+    # print(message)
+    # print(encrypted)
+    # print(decrypted)
     
 if __name__ == "__main__":
     main()
